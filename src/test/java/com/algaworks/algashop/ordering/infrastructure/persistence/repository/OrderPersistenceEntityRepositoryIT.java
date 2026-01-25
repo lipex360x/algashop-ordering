998e92005d56d5fbcef6eda776b9ee4ff2f7e436
Felipe Bambirra <lipex360@gmail.com>
2026-01-25 13:00:59 +0000

    audit properties added

--- src/test/java/com/algaworks/algashop/ordering/infrastructure/persistence/repository/OrderPersistenceEntityRepositoryIT.java
+++ src/test/java/com/algaworks/algashop/ordering/infrastructure/persistence/repository/OrderPersistenceEntityRepositoryIT.java
@@ -1,20 +1,19 @@
 package com.algaworks.algashop.ordering.infrastructure.persistence.repository;
 
-import com.algaworks.algashop.ordering.domain.model.utility.IdGenerator;
+import com.algaworks.algashop.ordering.infrastructure.persistence.config.SpringDataAuditingConfig;
 import com.algaworks.algashop.ordering.infrastructure.persistence.entity.OrderPersistenceEntity;
 import com.algaworks.algashop.ordering.infrastructure.persistence.entity.OrderPersistenceEntityDataBuilder;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-
-import java.math.BigDecimal;
-import java.time.OffsetDateTime;
+import org.springframework.context.annotation.Import;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Import(SpringDataAuditingConfig.class)
 class OrderPersistenceEntityRepositoryIT {
 
   @Autowired
@@ -31,6 +30,16 @@ class OrderPersistenceEntityRepositoryIT {
   void shouldCount() {
     long ordersCount = orderPersistenceEntityRepository.count();
     assertThat(ordersCount).isZero();
+  }
+
+  @Test
+  void shouldSetAuditingValues() {
+    OrderPersistenceEntity entity = OrderPersistenceEntityDataBuilder.existing().build();
+    entity = orderPersistenceEntityRepository.saveAndFlush(entity);
+
+    assertThat(entity.getCreatedByUserId()).isNotNull();
+    assertThat(entity.getLastModifiedAt()).isNotNull();
+    assertThat(entity.getLastModifiedByUserId()).isNotNull();
   }
 
 }
